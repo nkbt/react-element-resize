dc887b3dd376287a3afb41276e352daba65fb9dc
Nik Butenko <nik@butenko.me>
2016-05-23 13:52:53 +1000

    Updates

--- test/Component-test.js
+++
@@ -1,8 +0,0 @@
-import test from 'tape';
-import {ReactElementResize} from '../src/Component';
-
-
-test('ReactElementResize', t => {
-  t.ok(ReactElementResize instanceof Function, 'should be function');
-  t.end();
-});
